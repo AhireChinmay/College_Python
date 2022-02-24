52e663c5c27b4b3e52c549ca22ea320ef319b4db
AhireChinmay <chinmayahire3@gmail.com>
2022-02-24 12:03:46 +0530

    Commented in basicpay program

--- basic_pay.py
+++ basic_pay.py
@@ -1,9 +1,11 @@
+# this is  a program to find the basic pay of a person
 basic_pay = int(input("Enter your income in Rs: "))
 hra = (10/100)*basic_pay
 ta = (5/100)*basic_pay
 total_income = basic_pay-hra-ta
 tax = (2/100)*total_income
 net_income = total_income-tax
+
 print("Your HRA is",hra,"Rs")
 print("Your TA is",ta,"Rs")
 print("Your total income is",total_income,"Rs")
